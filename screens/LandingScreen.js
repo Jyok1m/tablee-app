0e6f44397f2a20edcf60d714ab35de2279f6eb06
Joachim Jasmin <joachim.jasmin@gmail.com>
2023-02-24 09:34:45 +0100

    Fixed the input and added style to the fields

--- screens/LandingScreen.js
+++ screens/LandingScreen.js
@@ -6,9 +6,18 @@ import {
   Image,
   TextInput,
 } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 
 export default function LandingScreen({ navigation }) {
+  const [usernameInput, setUsernameInput] = useState({
+    value: "",
+    isValid: true,
+  });
+  const [passwordInput, setPasswordInput] = useState({
+    value: "",
+    isValid: true,
+  });
+
   //Redirige vers la signup screen
   function signup() {
     navigation.navigate("SignupScreen");
@@ -16,8 +25,29 @@ export default function LandingScreen({ navigation }) {
 
   return (
     <View style={styles.container}>
-      <Image source={require("../public/logo.jpg")} />
-      <TextInput placeholder="Pseudo" />
+      <Image source={require("../public/logo.jpg")} style={styles.logo} />
+      <TextInput
+        placeholder="Pseudo"
+        textContentType="username"
+        style={
+          usernameInput.isValid ? styles.validInputBox : styles.invalidInputBox
+        }
+        onChangeText={(value) =>
+          setUsernameInput({ ...usernameInput, usernameInput: value })
+        }
+        value={usernameInput}
+      />
+      <TextInput
+        placeholder="Password"
+        textContentType="password"
+        style={
+          passwordInput.isValid ? styles.validInputBox : styles.invalidInputBox
+        }
+        onChangeText={(value) =>
+          setPasswordInput({ ...passwordInput, passwordInput: value })
+        }
+        value={passwordInput}
+      />
       <TouchableOpacity onPress={() => signup()}>
         <Text>LandingScreen</Text>
       </TouchableOpacity>
@@ -30,6 +60,34 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
+    justifyContent: "space-evenly",
+    paddingHorizontal: 20,
+  },
+  logo: {
+    width: "100%",
+    height: 300,
+    borderColor: "#CDAB82",
+    borderWidth: 3,
+    borderRadius: 5,
+  },
+  validInputBox: {
     justifyContent: "center",
+    maxPadding: 20,
+    width: "100%",
+    height: "7%",
+    backgroundColor: "#fff",
+    color: "#CDAB82",
+    borderColor: "#CDAB82",
+    borderWidth: 3,
+    borderRadius: 5,
+  },
+  invalidInputBox: {
+    maxPadding: 20,
+    width: "100%",
+    height: "7%",
+    backgroundColor: "#fff",
+    borderColor: "red",
+    borderWidth: 3,
+    borderRadius: 5,
   },
 });
