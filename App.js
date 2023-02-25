841c3c217dc7bd6d18328b624c946b8c9563abc9
Joachim Jasmin <joachim.jasmin@gmail.com>
2023-02-25 14:10:55 +0100

    Landing page done. Todo: test the signin function

--- App.js
+++ App.js
@@ -1,21 +1,47 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import LandingScreen from "./screens/LandingScreen";
 import ScanScreen from "./screens/ScanScreen";
 import SignupScreen from "./screens/SignupScreen";
 
+// Import store persistance modules:
+import { Provider } from "react-redux";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import user from "./reducers/user";
+
+import { persistStore, persistReducer } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+// Configure the store:
+const reducers = combineReducers({ user });
+const persistConfig = { key: "face-up", storage: AsyncStorage };
+const store = configureStore({
+  reducer: persistReducer(persistConfig, reducers),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }),
+});
+const persistor = persistStore(store);
+
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="LandingScreen" component={LandingScreen} />
-        <Stack.Screen name="SignupScreen" component={SignupScreen} />
-        <Stack.Screen name="ScanScreen" component={ScanScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <SafeAreaProvider>
+          <NavigationContainer>
+            <Stack.Navigator screenOptions={{ headerShown: false }}>
+              <Stack.Screen name="LandingScreen" component={LandingScreen} />
+              <Stack.Screen name="SignupScreen" component={SignupScreen} />
+              <Stack.Screen name="ScanScreen" component={ScanScreen} />
+            </Stack.Navigator>
+          </NavigationContainer>
+        </SafeAreaProvider>
+      </PersistGate>
+    </Provider>
   );
 }
--- screens/LandingScreen.js
+++ screens/LandingScreen.js
@@ -5,89 +5,186 @@ import {
   TouchableOpacity,
   Image,
   TextInput,
+  KeyboardAvoidingView,
 } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import React, { useState } from "react";
 
+import { useDispatch } from "react-redux";
+import { signinUser } from "../reducers/user";
+
 export default function LandingScreen({ navigation }) {
+  const dispatch = useDispatch();
   const [usernameInput, setUsernameInput] = useState({
     value: "",
     isValid: true,
+    error: "",
   });
   const [passwordInput, setPasswordInput] = useState({
     value: "",
     isValid: true,
+    error: "",
   });
+  const [field, setField] = useState({
+    isMissing: false,
+    error: "",
+  });
+
+  //Fonction signin pour login le user
+  async function signin() {
+    const username = usernameInput.value;
+    const password = passwordInput.value;
+    const userData = { username, password };
+
+    setUsernameInput({ ...usernameInput, isValid: true, error: "" });
+    setPasswordInput({ ...passwordInput, isValid: true, error: "" });
+    setField({ isMissing: false, error: "" });
+
+    const response = await fetch(
+      "https://tablee-backend.vercel.app/users/signin",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(userData),
+      }
+    );
+    const data = await response.json();
+    if (data.result === true) {
+      dispatch(signinUser({ username, token: data.token }));
+      setUsernameInput({ value: "", isValid: true, error: "" });
+      setPasswordInput({ value: "", isValid: true, error: "" });
+      setField({ isMissing: false, error: "" });
+    } else if (data.errorSrc === "username") {
+      setUsernameInput({ ...usernameInput, isValid: false, error: data.error });
+    } else if (data.errorSrc === "password") {
+      setPasswordInput({ ...passwordInput, isValid: false, error: data.error });
+    } else {
+      setField({ isMissing: true, error: data.error });
+    }
+  }
 
   //Redirige vers la signup screen
   function signup() {
     navigation.navigate("SignupScreen");
   }
 
+  let errorMessage = <View />;
+  if (field.isMissing) {
+    errorMessage = <Text style={styles.errorText}>{field.error}</Text>;
+  } else if (usernameInput.isValid === false) {
+    errorMessage = <Text style={styles.errorText}>{usernameInput.error}</Text>;
+  } else if (passwordInput.isValid === false) {
+    errorMessage = <Text style={styles.errorText}>{passwordInput.error}</Text>;
+  }
+
   return (
-    <View style={styles.container}>
-      <Image source={require("../public/logo.jpg")} style={styles.logo} />
+    <SafeAreaView style={styles.container}>
+      <Image source={require("../assets/logo.jpg")} style={styles.logo} />
+      {errorMessage}
       <TextInput
-        placeholder="Pseudo"
+        placeholder="Nom d'utilisateur"
+        value={usernameInput}
         textContentType="username"
         style={
           usernameInput.isValid ? styles.validInputBox : styles.invalidInputBox
         }
-        onChangeText={(value) =>
-          setUsernameInput({ ...usernameInput, usernameInput: value })
-        }
-        value={usernameInput}
+        onChangeText={(value) => setUsernameInput({ ...usernameInput, value })}
+        onFocus={() => setField({ isMissing: false, error: "data.error" })}
       />
       <TextInput
-        placeholder="Password"
+        placeholder="Mot de passe"
+        value={passwordInput}
         textContentType="password"
+        secureTextEntry={true}
         style={
           passwordInput.isValid ? styles.validInputBox : styles.invalidInputBox
         }
-        onChangeText={(value) =>
-          setPasswordInput({ ...passwordInput, passwordInput: value })
-        }
-        value={passwordInput}
+        onChangeText={(value) => setPasswordInput({ ...passwordInput, value })}
+        onFocus={() => setField({ isMissing: false, error: "data.error" })}
       />
-      <TouchableOpacity onPress={() => signup()}>
-        <Text>LandingScreen</Text>
+      <TouchableOpacity onPress={() => signin()} style={styles.button}>
+        <Text style={styles.text}>Connection</Text>
       </TouchableOpacity>
-    </View>
+      <TouchableOpacity onPress={() => signup()} style={styles.button}>
+        <Text style={styles.text}>S'enregistrer</Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Text style={styles.pressableText}>Mot de passe oublié</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "space-evenly",
     paddingHorizontal: 20,
+    backgroundColor: "#1D2C3B",
   },
   logo: {
     width: "100%",
-    height: 300,
+    maxHeight: "40%",
     borderColor: "#CDAB82",
     borderWidth: 3,
     borderRadius: 5,
+    marginTop: 20,
+  },
+  textContainer: {
+    width: "100%",
+    height: "40%",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+    backgroundColor: "#1D2C3B",
   },
   validInputBox: {
-    justifyContent: "center",
-    maxPadding: 20,
+    paddingHorizontal: 20,
     width: "100%",
-    height: "7%",
+    minHeight: "7%",
     backgroundColor: "#fff",
-    color: "#CDAB82",
     borderColor: "#CDAB82",
     borderWidth: 3,
     borderRadius: 5,
+    marginTop: 20,
+    fontSize: 16,
   },
   invalidInputBox: {
-    maxPadding: 20,
+    paddingHorizontal: 20,
     width: "100%",
-    height: "7%",
+    minHeight: "7%",
     backgroundColor: "#fff",
     borderColor: "red",
     borderWidth: 3,
     borderRadius: 5,
+    marginTop: 20,
+    fontSize: 16,
+  },
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+    paddingHorizontal: 20,
+    width: "100%",
+    minHeight: "7%",
+    backgroundColor: "#CDAB82",
+    borderColor: "#CDAB82",
+    borderWidth: 3,
+    borderRadius: 5,
+    marginTop: 20,
+  },
+  pressableText: {
+    textDecorationLine: "underline",
+    color: "#CDAB82",
+    marginTop: 20,
+    fontSize: 16,
+  },
+  errorText: {
+    color: "red",
+    fontWeight: "bold",
+    marginTop: 20,
+    fontSize: 16,
+  },
+  text: {
+    fontSize: 16,
+    fontWeight: "bold",
   },
 });
