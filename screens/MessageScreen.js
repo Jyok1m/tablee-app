83c447f8b73e585785fcd858910ce2d4128bbdd9
Andrea <and29ferraro@gmail.com>
2023-03-07 17:12:00 +0100

    Enregistrement des msg en bdd et récupération

--- screens/MessageScreen.js
+++ screens/MessageScreen.js
@@ -5,34 +5,29 @@ import {
   Button,
   TextInput,
   TouchableOpacity,
-  FlatList,
   Pressable,
-  SafeAreaView
+  SafeAreaView,
+  ScrollView,
 } from "react-native";
 import React from "react";
 import Header from "../components/Header";
 import { useEffect, useState, useLayoutEffect } from "react";
 import { BACKEND_URL } from "../backend_url";
 import { io } from "socket.io-client";
-import { useDispatch, useSelector } from "react-redux";
-import MessageComponent from "../components/MessageComponent";
+import { useSelector } from "react-redux";
+import { Ionicons } from "@expo/vector-icons";
 
-var socket = io(`http://192.168.10.161:3000`);
+var socket = io(`http://192.168.1.10:3000`);
 
 export default function MessageScreen({ route, navigation }) {
   const [message, setMessage] = useState("");
-  const [messageInput, setMessageInput] = useState("");
   const [messageToSend, setMessageToSend] = useState("");
   const [user, setUser] = useState("");
+  const [chatMessages, setChatMessages] = useState([]);
+  const [roomNumber, setRoomNumber] = useState("");
 
-    // Recupere les infos de l'utilisateur
+  // Recupere les infos de l'utilisateur
   const userInfos = useSelector((state) => state.user.value);
-
-  const [chatMessages, setChatMessages] = useState([
-
-  ]);
-
-
 
   // Recupere le nom et l'id de la chatroom
   const { name, id } = route.params;
@@ -49,66 +44,117 @@ export default function MessageScreen({ route, navigation }) {
 
   // Met le titre du header avec le nom de la chatroom
   useLayoutEffect(() => {
-    navigation.setOptions({ title: name });
+    navigation.setOptions({ title: name, roomId: id });
+    setRoomNumber(id);
     getUsername();
   }, []);
 
-  // Recupere l'heure d'envoi du message puis log l'username, le message et le timestamp
- 
 
   // Gere l'envoie de messages
   useEffect(() => {
     socket.on("sendMessageFromBack", (newMessage) => {
-      setMessage(newMessage);
-      console.log(message)
+      console.log(newMessage);
     });
+
+    // Recupere les messages du chat en bdd
+    (async () => {
+      const response = await fetch(
+        `http://192.168.1.10:3000/messages/chatRoom/${id}`
+      );
+      const data = await response.json();
+      if (data) {
+        setChatMessages(data.chat);
+      }
+    })();
   }, [message]);
 
-  async function sendMessage() {
-    setMessageToSend(message);
-        const hour =
-          new Date().getHours() < 10
-            ? `0${new Date().getHours()}`
-            : `${new Date().getHours()}`;
-
-        const min =
-          new Date().getMinutes() < 10
-            ? `0${new Date().getMinutes()}`
-            : `${new Date().getMinutes()}`;
-
-        //console.log(message, user, hour, min);
-    //socket.emit("sendMessage", messageToSend);
-    socket.emit("newMessage", {
-      messageToSend,
-      room_id: id,
-      user,
-      timestamp: { hour, min },
-    });
-  }
-
-  console.log(messageToSend)
-
-  return (
-
-    <SafeAreaView style={styles.messagingscreen}>
-    <Text>{name}</Text>
+  // Render les messages du chat
+  let chatMessagesToRender = <View></View>;
+  chatMessagesToRender = chatMessages?.map((data, i) => {
+    const status = data.user !== userInfos.username;
+    return (
       <View
+        key={i}
         style={[
           styles.messagingscreen,
           { paddingVertical: 15, paddingHorizontal: 10 },
         ]}>
-        {chatMessages[0] ? (
-          <FlatList
-            data={chatMessages}
-            renderItem={({ item }) => (
-              <MessageComponent item={item} user={user} />
-            )}
-            keyExtractor={(item) => item.id}
-          />
-        ) : (
-          ""
-        )}
+        <View>
+          <View
+            style={
+              status
+                ? styles.mmessageWrapper
+                : [styles.mmessageWrapper, { alignItems: "flex-end" }]
+            }>
+            <View style={{ flexDirection: "row", alignItems: "center" }}>
+              <Ionicons
+                name="person-circle-outline"
+                size={30}
+                color="black"
+                style={styles.mavatar}
+              />
+              <View
+                style={
+                  status
+                    ? styles.mmessage
+                    : [
+                        styles.mmessage,
+                        { backgroundColor: "rgb(194, 243, 194)" },
+                      ]
+                }>
+                <Text>{data.message}</Text>
+              </View>
+            </View>
+            <Text style={{ marginLeft: 40 }}>{data.time}</Text>
+          </View>
+        </View>
       </View>
+    );
+  });
+
+  async function sendMessage() {
+
+    socket.emit("sendMessage", "Hello John !");
+
+    //Recupére l'heure d'envoi
+    const hour =
+      new Date().getHours() < 10
+        ? `0${new Date().getHours()}`
+        : `${new Date().getHours()}`;
+
+    const min =
+      new Date().getMinutes() < 10
+        ? `0${new Date().getMinutes()}`
+        : `${new Date().getMinutes()}`;
+
+        // Envoi message vers le back
+    //socket.emit("newMessage", message);
+
+
+
+    // Envoi le message dans la bdd
+    (async () => {
+      await fetch(`http://192.168.1.10:3000/messages/send`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          message: message,
+          roomId: roomNumber,
+          user: user,
+          date: { hour, min },
+        }),
+      });
+      
+  
+    })();
+   
+  }
+
+
+  return (
+    <SafeAreaView style={styles.messagingscreen}>
+      <Text>{name}</Text>
+      <ScrollView>{chatMessagesToRender}</ScrollView>
 
       <View style={styles.messaginginputContainer}>
         <TextInput
@@ -151,6 +197,51 @@ const styles = StyleSheet.create({
     transition: 1,
     width: "50%",
     alignItems: "center",
+  },
+  messaginginputContainer: {
+    width: "100%",
+    minHeight: 100,
+    backgroundColor: "white",
+    paddingVertical: 30,
+    paddingHorizontal: 15,
+    justifyContent: "center",
+    flexDirection: "row",
+  },
+  messaginginput: {
+    borderWidth: 1,
+    padding: 15,
+    flex: 1,
+    marginRight: 10,
+    borderRadius: 20,
+  },
+  messagingbuttonContainer: {
+    width: "30%",
+    backgroundColor: "green",
+    borderRadius: 3,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 50,
+  },
+
+  mmessageWrapper: {
+    width: "100%",
+    alignItems: "flex-start",
+    marginBottom: 15,
+  },
+  mmessage: {
+    maxWidth: "50%",
+    backgroundColor: "#f5ccc2",
+    padding: 15,
+    borderRadius: 10,
+    marginBottom: 2,
+  },
+  mvatar: {
+    marginRight: 5,
+  },
+  // Bulles messages
+  chatemptyText: { fontWeight: "bold", fontSize: 24, paddingBottom: 30 },
+  messagingscreen: {
+    flex: 1,
   },
   messaginginputContainer: {
     width: "100%",
