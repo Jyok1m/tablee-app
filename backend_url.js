54a07894c2a3edba6d9765947612f10d5de198c8
Joachim Jasmin <joachim.jasmin@gmail.com>
2023-03-09 16:10:47 +0100

    modified checkout message

--- backend_url.js
+++ backend_url.js
@@ -1,4 +1,4 @@
- export const BACKEND_URL = "https://tablee-backend.vercel.app"; // Vercel
+export const BACKEND_URL = "https://tablee-backend.vercel.app"; // Vercel
 // export const BACKEND_URL = "http://192.168.10.125:3000"; // Capsule
 // export const BACKEND_URL = "http://192.168.79.133:3000"; // Maison
 // export const BACKEND_URL = "http://192.168.10.160:3000"; // Andrea Capsule
