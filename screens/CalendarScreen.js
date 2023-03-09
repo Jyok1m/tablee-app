44c3a34595a016431ae68e574d88775da2274c4e
Joachim Jasmin <joachim.jasmin@gmail.com>
2023-03-09 15:57:22 +0100

    fixed bugs

--- screens/CalendarScreen.js
+++ screens/CalendarScreen.js
@@ -260,7 +260,7 @@ const styles = StyleSheet.create({
   },
   inputCard: {
     width: "100%",
-    height: 225,
+    maxHeight: 300,
     backgroundColor: "transparent",
     borderColor: "#CDAB82",
     borderWidth: 1,
